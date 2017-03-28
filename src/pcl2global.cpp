677c89ead380f20e41b2ac3431671e78cfd76680
Madhav Patel <patel.v.madhav@gmail.com>
2017-03-28 00:15:02 -0400

    fixed pcl to map

---
+++ src/pcl2global.cpp
@@ -0,0 +1,60 @@
+#include "ros/ros.h"
+#include "tf/transform_listener.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
+#include "laser_geometry/laser_geometry.h"
+#include "pcl_ros/transforms.h"
+#include "pcl/point_types.h"
+
+
+/*class PointCloudTransformer{
+
+public:
+  ros::NodeHandle n_;
+  tf::TransformListener listener_;
+  ros::Publisher publisher;
+
+  PointCloudTransformer(ros::NodeHandle n) :
+    n_(n)
+  {
+    ROS_INFO("Transformer Init");
+    n_.subscribe<sensor_msgs::PointCloud2>("cloud_out", 10, boost::bind(&PointCloudTransformer::callback, this, _1));
+    publisher = n_.advertise<sensor_msgs::PointCloud2>("/cloud_out_global",10);
+  }
+
+  void callback (const sensor_msgs::PointCloud2::ConstPtr& pcl_in)
+  {
+    ROS_INFO("In callback");
+    sensor_msgs::PointCloud2 pcl_out;
+
+    listener_.waitForTransform("/odom", (*pcl_in).header.frame_id, (*pcl_in).header.stamp, ros::Duration(5.0));
+    pcl_ros::transformPointCloud("/odom", *pcl_in, pcl_out, listener_);
+
+    publisher.publish(pcl_out);
+
+  }
+};*/
+tf::TransformListener* tf_listener;
+ros::Publisher tf_pub;
+
+void callback(const sensor_msgs::PointCloud2::ConstPtr& pcl_in)
+{
+    sensor_msgs::PointCloud2 pcl_out;
+    tf_listener->waitForTransform("/odom", (*pcl_in).header.frame_id, (*pcl_in).header.stamp, ros::Duration(5.0));
+    pcl_ros::transformPointCloud("/odom", *pcl_in, pcl_out, *tf_listener);
+    tf_pub.publish(pcl_out);
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "point_cloud_to_map");
+  ros::NodeHandle n;
+  tf_listener = new tf::TransformListener();
+  ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>("/cloud_out", 1, callback);
+  tf_pub = n.advertise<sensor_msgs::PointCloud2> ("/cloud_out_global", 1);
+
+  ros::spin();
+
+  return 0;
+}
--- src/scan2pcl.cpp
+++ src/scan2pcl.cpp
@@ -19,7 +19,7 @@ public:
   LaserScanToPointCloud(ros::NodeHandle n) :
     n_(n),
     laser_sub_(n_, "/scan", 10),
-    laser_notifier_(laser_sub_,listener_, "/map", 10)
+    laser_notifier_(laser_sub_,listener_, "/base_link", 10)
   {
     laser_notifier_.registerCallback(
       boost::bind(&LaserScanToPointCloud::scanCallback, this, _1));
